9b175eff655288b9394b0c1c65330a8aadad93ce
Jonathan <qaovxtazypdl@hotmail.com>
2015-04-18 01:43:01 -0400

    Implemented overlay and overlay saving. Fixed a few small bugs. Added premultiply flag to constructor to handle the two scenarios.
    Change-Id: I9522470b45a3b94daec5ed4b093691ebe2ae4894
    Signed-off-by: Jonathan <qaovxtazypdl@hotmail.com>

--- src/ImageProcessing.java
+++ src/ImageProcessing.java
@@ -12,8 +12,22 @@ class ImageProcessing
 	private int _height, _bgHeight;
 	private int _width, _bgWidth;
 	
-	public ImageProcessing(String imageName, String bgName)
+	private int _timesSaved;
+	private int _imageType;
+	private boolean _premultiply;
+	
+	public ImageProcessing(String imageName, boolean premultiply)
 	{
+		_premultiply = premultiply;
+		if(_premultiply)
+		{
+			_imageType = BufferedImage.TYPE_INT_ARGB_PRE;
+		}
+		else
+		{
+			_imageType = BufferedImage.TYPE_INT_ARGB;
+		}
+		
 		_imageName = imageName;
 		_image = null;
 		File inputFile = new File(_imageName);
@@ -27,9 +41,9 @@ class ImageProcessing
 		_height = _image.getHeight();
 		_width = _image.getWidth();
 		
-		if(_image.getType() != BufferedImage.TYPE_INT_ARGB_PRE)
+		if(_image.getType() != _imageType)
 		{
-			BufferedImage argbImage = new BufferedImage(_width, _height, BufferedImage.TYPE_INT_ARGB_PRE);
+			BufferedImage argbImage = new BufferedImage(_width, _height, _imageType);
 	        for(int w = 0; w < _width; w++)
 	        {
 		        for(int h = 0; h < _height; h++)
@@ -39,41 +53,13 @@ class ImageProcessing
 	        }
 	        _image = argbImage;
 		}
-		
-		_bgName = bgName;
-		_bg = null;
-		inputFile = new File(_bgName);
-		try {
-			_bg = ImageIO.read(inputFile);
-		} catch (IOException e) {
-			System.out.println("Could not read input image.");
-			return;
-		}
-			
-		_bgHeight = _bg.getHeight();
-		_bgWidth = _bg.getWidth();
-		
-		if(_bg.getType() != BufferedImage.TYPE_INT_ARGB_PRE)
-		{
-			BufferedImage argbImage = new BufferedImage(_bgWidth, _bgHeight, BufferedImage.TYPE_INT_ARGB_PRE);
-	        for(int w = 0; w < _bgWidth; w++)
-	        {
-		        for(int h = 0; h < _bgHeight; h++)
-		        {
-		        	argbImage.setRGB(w, h, (_bg.getRGB(w, h) | 0xff000000)); //will break on transparent input
-		        }
-	        }
-	        _bg = argbImage;
-		}
 	}
 	
 	//returns how "different" the color is. Closer it is, more transparent it will be.
 	private int getColorDifference(int base_r, int base_g, int base_b, int r, int g, int b) {
-		return Math.max(Math.abs(base_r - r), Math.max(Math.abs(base_g - g), Math.abs(base_b - b)));
-    	//return (int) (Math.abs(base_r - r) + Math.abs(base_g - g) + Math.abs(base_b - b));
+		return Math.max(Math.abs(base_r - r), Math.max(Math.abs(base_g - g), Math.abs(base_b - b))); //max brightness metric
+    	//return (int) (0.2126 * Math.abs(base_r - r) + 0.7152 * Math.abs(base_g - g) + 0.0722 * Math.abs(base_b - b)); //luminance metric
 	}
-	
-
 	
 	public void RGBtoALPHA(int base_rgb) {
 		if(_image == null) return;
@@ -92,58 +78,79 @@ class ImageProcessing
             	int b = (argb) & 0xff;
             	int newa = getColorDifference(base_r, base_g, base_b, r, g, b); //choose an a.
 
+            	if(_premultiply)
+            	{
+                	r = r * a / 0xff;
+                	g = g * a / 0xff;
+                	b = b * a / 0xff;
+            		double a_ratio = (double)newa / 0xff;
 
-            	//////////////////////////COLOR TUNING - PREMULTIPLIED//////////////////////////       
-            	/*r = r * a / 0xff;
-            	g = g * a / 0xff;
-            	b = b * a / 0xff;
-        		double a_ratio = (double)newa / 0xff;
+                	//input = transparency + (backgd * (1.0-transparency.a))
+                	//transparency = input - (backgd * (1.0-transparency.a))
+            		a = (int) ((a - (1-a_ratio) * base_a));
+            		r = (int) ((r - (1-a_ratio) * base_r));
+            		g = (int) ((g - (1-a_ratio) * base_g));
+            		b = (int) ((b - (1-a_ratio) * base_b));
 
-            	//input = transparency + (backgd * (1.0-transparency.a))
-            	//transparency = input - (backgd * (1.0-transparency.a))
-        		a = (int) ((a - (1-a_ratio) * base_a));
-        		r = (int) ((r - (1-a_ratio) * base_r));
-        		g = (int) ((g - (1-a_ratio) * base_g));
-        		b = (int) ((b - (1-a_ratio) * base_b));
+            		r = r > 0xff? 0xff : r;
+            		b = b > 0xff? 0xff : b;
+            		g = g > 0xff? 0xff : g;
+            		a = a > 0xff? 0xff : a;
+            		
+                	argb = (a << 24) | (r << 16) | (g << 8) | (b);
+            	}
+            	else
+            	{
+                	double base_a_ratio = (double)a / 0xff;
+            		double a_ratio = (double)newa / 0xff;
+            		
+                	//input = transparency.a * transparency + (backgd * (1.0-transparency.a))
+                	//transparency = input - ((1-transparency.a) * backgd) / transparency.a
+            		a = (int) ((base_a_ratio * a - (1-a_ratio) * base_a));
+            		r = (int) ((base_a_ratio * r - (1-a_ratio) * base_r) / a_ratio);
+            		g = (int) ((base_a_ratio * g - (1-a_ratio) * base_g) / a_ratio);
+            		b = (int) ((base_a_ratio * b - (1-a_ratio) * base_b) / a_ratio);
 
-        		r = r > 0xff? 0xff : r;
-        		b = b > 0xff? 0xff : b;
-        		g = g > 0xff? 0xff : g;
-        		a = a > 0xff? 0xff : a;*/
-            	//////////////////////////COLOR TUNING - PREMULTIPLIED//////////////////////////
-            	
-            	//////////////////////////COLOR TUNING - nonpremultiuplied//////////////////////
-            	
-            	double base_a_ratio = (double)a / 0xff;
-        		double a_ratio = (double)newa / 0xff;
-            	//input = transparency.a * transparency + (backgd * (1.0-transparency.a))
-            	//transparency = input - ((1-transparency.a) * backgd) / transparency.a
-        		a = (int) ((base_a_ratio * a - (1-a_ratio) * base_a));
-        		r = (int) ((base_a_ratio * r - (1-a_ratio) * base_r) / a_ratio);
-        		g = (int) ((base_a_ratio * g - (1-a_ratio) * base_g) / a_ratio);
-        		b = (int) ((base_a_ratio * b - (1-a_ratio) * base_b) / a_ratio);
-
-        		r = r > 0xff? 0xff : r;
-        		b = b > 0xff? 0xff : b;
-        		g = g > 0xff? 0xff : g;
-        		a = a > 0xff? 0xff : a;
-        		
-            	//////////////////////////COLOR TUNING - nonpremultiuplied//////////////////////
+            		r = r > 0xff? 0xff : r;
+            		b = b > 0xff? 0xff : b;
+            		g = g > 0xff? 0xff : g;
+            		a = a > 0xff? 0xff : a;
+            		
+                	argb = (a << 24) | (r << 16) | (g << 8) | (b);
+            	}
 
             	//enter the r' back into the pixel.
-            	argb = (a << 24) | (r << 16) | (g << 8) | (b);
             	_image.setRGB(w,h, argb);
 	        }
         }
 	}
 	
-	//get a better algorithm for zoomins.
-	private int GetScaledPixel(BufferedImage image, int scaledMaxX, int scaledMaxY, int x, int y) {
-		//TODO:
-		return image.getRGB(x, y);
-	}
-	
-	public void OverlayImage() {
+	public void OverlayImage(String bgName) {
+		_bgName = bgName;
+		_bg = null;
+		File inputFile = new File(_bgName);
+		try {
+			_bg = ImageIO.read(inputFile);
+		} catch (IOException e) {
+			System.out.println("Could not read input image.");
+			return;
+		}
+			
+		_bgHeight = _bg.getHeight();
+		_bgWidth = _bg.getWidth();
+		
+		if(_bg.getType() != _imageType)
+		{
+			BufferedImage argbImage = new BufferedImage(_bgWidth, _bgHeight, _imageType);
+	        for(int w = 0; w < _bgWidth; w++)
+	        {
+		        for(int h = 0; h < _bgHeight; h++)
+		        {
+		        	argbImage.setRGB(w, h, (_bg.getRGB(w, h) | 0xff000000)); //will break on transparent input
+		        }
+	        }
+	        _bg = argbImage;
+		}
 		if(_bg == null) return;
 		
         for(int w = 0; w < _bgWidth; w++) {
@@ -158,37 +165,38 @@ class ImageProcessing
             	int src_r = (src_argb >>> 16) & 0xff;
             	int src_g = (src_argb >>> 8) & 0xff;
             	int src_b = (src_argb) & 0xff;
+            	int argb = 0;
             	
-            	//////////////////////////COLOR TUNING - PREMULTIPLIED//////////////////////////       
-            	//pix = transparency + (backgd * (1.0-transparency.a))
-            	/*
-            	int a = src_a + bg_a * (0xff - src_a) / 0xff;
-            	int r = src_r + bg_r * (0xff - src_a) / 0xff;
-            	int g = src_g + bg_g * (0xff - src_a) / 0xff;
-            	int b = src_b + bg_b * (0xff - src_a) / 0xff;
-            	*/
-            	//////////////////////////COLOR TUNING - PREMULTIPLIED//////////////////////////
+            	if(_premultiply)
+            	{
+                	//pix = transparency + (backgd * (1.0-transparency.a))
+                	int a = src_a + bg_a * (0xff - src_a) / 0xff;
+                	int r = src_r + bg_r * (0xff - src_a) / 0xff;
+                	int g = src_g + bg_g * (0xff - src_a) / 0xff;
+                	int b = src_b + bg_b * (0xff - src_a) / 0xff;
+                	
+                	argb = (a << 24) | (r << 16) | (g << 8) | (b);
+            	}
+            	else
+            	{
+                	//pix = transparency.a * transparency + (backgd * (1.0-transparency.a))
+                	double a_ratio = (double) src_a / 0xff;
+                	int a = (int) (src_a + (1 - a_ratio) * bg_a);
+                	int r = (int) (a_ratio * src_r + (1 - a_ratio) * bg_r);
+                	int g = (int) (a_ratio * src_g + (1 - a_ratio) * bg_g);
+                	int b = (int) (a_ratio * src_b + (1 - a_ratio) * bg_b);
+                	
+                	argb = (a << 24) | (r << 16) | (g << 8) | (b);
+            	}
             	
-            	//////////////////////////COLOR TUNING - nonpremultiuplied//////////////////////
-            	//pix = transparency.a * transparency + (backgd * (1.0-transparency.a))
-            	double a_ratio = (double) src_a / 0xff;
-            	int a = (int) (src_a + (1 - a_ratio) * bg_a);
-            	int r = (int) (a_ratio * src_r + (1 - a_ratio) * bg_r);
-            	int g = (int) (a_ratio * src_g + (1 - a_ratio) * bg_g);
-            	int b = (int) (a_ratio * src_b + (1 - a_ratio) * bg_b);
-            	
-            	/*
-        		          a + (1-a_ratio) * base_a = (base_a_ratio * a);
-        		a_ratio * r + (1-a_ratio) * base_r = (base_a_ratio * r);
-        		a_ratio * g + (1-a_ratio) * base_g = (base_a_ratio * g);
-        		a_ratio * b + (1-a_ratio) * base_b = (base_a_ratio * b);
-        		*/
-            	//////////////////////////COLOR TUNING - nonpremultiuplied//////////////////////
-            	
-            	int argb = (a << 24) | (r << 16) | (g << 8) | (b);
+            	//enter the r' back into the pixel.
             	_bg.setRGB(w,h, argb);
 	        }
         }
+        //swap the new image to the main position.
+        _image = _bg;
+        _width = _bgWidth;
+        _height = _bgHeight;
 	}
 	
 	public boolean Save(String outName)
@@ -202,16 +210,13 @@ class ImageProcessing
 		try {
 			if(_image != null)
 			{
-		        File deblendedFileName = new File(outName + "_deblended.png");
-				success &= ImageIO.write(_image, "png", deblendedFileName);
+		        File fileName = new File(outName + "_" + _timesSaved++ + ".png");
+				success &= ImageIO.write(_image, "png", fileName);
 			}
-			if(_bg != null)
+			else 
 			{
-				File outputFile = new File(outName + ".png");
-				success &= ImageIO.write(_bg, "png", outputFile);
-			}
-			if(_image == null || _bg == null) 
 				success = false;
+			}
 		} catch (IOException e) {
 			System.out.println("Could not save processed image.");
 			success = false;
@@ -221,6 +226,12 @@ class ImageProcessing
 	
 	public boolean Save()
 	{
-		return Save(_imageName.substring(0, _imageName.lastIndexOf('.')) + "_temp");
+		return Save(_imageName.substring(0, _imageName.lastIndexOf('.')) + "_default");
+	}
+	
+	//get a better algorithm for zoomins.
+	private int GetScaledPixel(BufferedImage image, int scaledMaxX, int scaledMaxY, int x, int y) {
+		//TODO:
+		return image.getRGB(x, y);
 	}
 }
--- src/Transparency.java
+++ src/Transparency.java
@@ -2,18 +2,21 @@ class Transparency
 {
 	public static void main(String[] args)
 	{
-		ImageProcessing image = new ImageProcessing("X:\\Desktop\\TEST\\4.jpg", "X:\\Desktop\\TEST\\run.png");
+		ImageProcessing image = new ImageProcessing("X:\\Desktop\\TEST\\4.jpg", true);
 		image.RGBtoALPHA(0xff000000);
-		image.OverlayImage();
 		image.Save();
+		image.OverlayImage("X:\\Desktop\\TEST\\run.png");		
+		image.Save();
+
 	}
 }
 
-//todo: brighten image that has been alpha'd. Color tuning.
-//todo: better algorithm for scaling images.
-//todo": optimize
-//todo: parameters - 
+//TODO: optimize
+//TODO: better algorithm for scaling images. scale images. handle output size
+//TODO: parameters - 
 //			-auto AUTO determine background color
 //			-i input file, -o output file
 //			-bg background overlay file
-//			-nopre for no precalculated alpha (consistent with image editor formats...)
+//			-pre for premultiplied alpha (inconsistent with image editor formats...)
+//		    -overlay to overlay an existing file directly.
+//			-osize specify output file dimensions
